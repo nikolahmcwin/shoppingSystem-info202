d4b3b8e5bfd4d1cdc8a2af099cb15a49ef7e36a5
Nikolah Pearce <peani371@student.otago.ac.nz>
2018-07-16 10:47:20 +1200

    Implemented Product domain class. References issue #1.

---
+++ src/main/java/domain/Product.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author peani371
+ */
+public class Product {
+    
+    private String productID;
+    private String name;
+    private String description;
+    private String category;
+    private BigDecimal price;
+    private Integer quantityInStock;
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "productID=" + productID + ", name=" + name + ", description=" + description + ", category=" + category + ", price=" + price + ", quantityInStock=" + quantityInStock + '}';
+    }
+    
+}
