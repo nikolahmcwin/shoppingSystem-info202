4f9dbbaa821da11e020755b9ca9b6f3be6b93bef
peani371 <peani371@mds-xa101s-p076.vdi.student.otago.ac.nz>
2018-07-21 18:01:20 +1200

    Implemented relationships/methods for Sale class. References #1

--- src/main/java/domain/Sale.java
+++ src/main/java/domain/Sale.java
@@ -1,7 +1,9 @@
 package domain;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -14,6 +16,8 @@ public class Sale {
 	private Date date;
 	// Would status be better off as a boolean?
 	private String status;
+	private Customer customer;
+	private List<SaleItem> saleItems = new ArrayList<>();
 
 	public String getSaleID() {
 		return saleID;
@@ -25,6 +29,10 @@ public class Sale {
 
 	public String getStatus() {
 		return status;
+	}
+
+	public Customer getCustomer() {
+		return customer;
 	}
 
 	public void setSaleID(String saleID) {
@@ -39,18 +47,26 @@ public class Sale {
 		this.status = status;
 	}
 
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
 	@Override
 	public String toString() {
 		return "Sale{" + "saleID=" + saleID + ", date=" + date + ", status=" + status + '}';
 	}
 	
 	public BigDecimal getTotal() {
-		
-		return null;
+		BigDecimal total = BigDecimal.ZERO;
+		// Loop through each saleItem, and pull out the price, adding to total
+		for (SaleItem item : saleItems) {
+			total.add(item.getItemTotal());
+		}
+		return total;
 	}
 	
-	public void addItem() {
-		
+	public void addItem(SaleItem item) {
+		saleItems.add(item);
 	}
 	
 }
