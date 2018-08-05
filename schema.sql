b73dfb397ad5c2ca986eb07b882a1c7d6ca640bc
Nikolah Pearce <peani371@student.otago.ac.nz>
2018-08-06 10:27:31 +1200

    Created schema sql file. References #9

---
+++ schema.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  peani371
+ * Created: 6/08/2018
+ */
+
+CREATE TABLE Product (
+    PID varchar(20),
+    Pname varchar(50) not null,
+    Description varchar(150),
+    Category varchar(50),
+    Price decimal(7,2),
+    Quantity integer,
+    constraint Product_PK primary key (PID)
+);
