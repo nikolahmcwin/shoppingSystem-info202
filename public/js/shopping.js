46622b105843c45223272d067d331ef8045acf1a
peani371 <peani371@mds-xa101s-p018.vdi.student.otago.ac.nz>
2018-09-08 15:33:50 +1200

    Implemented customer registration controller #18

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -11,6 +11,10 @@ module.factory('categoryDAO', function ($resource) {
     return $resource('/api/categories/:cat');
 });
 
+module.factory('registerDAO', function ($resource) {
+    return $resource('/api/register');
+});
+
 // Product controller
 module.controller('ProductController', function (productDAO, categoryDAO) {
 
@@ -24,14 +28,22 @@ module.controller('ProductController', function (productDAO, categoryDAO) {
     this.selectCategory = function (selectedCat) {
         this.products = categoryDAO.query({"cat": selectedCat});
     };
-    
+
     this.displayAll = function () {
         this.products = productDAO.query();
     };
-    
+
 });
 
 // Customer controller
+module.controller('CustomerController', function (registerDAO) {
+    this.customers = registerDAO.query();
+    this.registerCustomer = function (customer) {
+        registerDAO.save(null, customer);
+        console.log(customer);
+    };
+
+});
 
 
 // Shopping cart controller
