947541126a044a7de8991c8c8dbd7daee761e631
peani371 <peani371@mds-xa101s-p018.vdi.student.otago.ac.nz>
2018-09-08 14:39:10 +1200

    Implemented product controller #18

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -2,15 +2,36 @@
 
 var module = angular.module('ShoppingApp', ['ngResource', 'ngStorage']);
 
+// factories
 module.factory('productDAO', function ($resource) {
     return $resource('/api/products/:id');
 });
 
-// Product controller
-module.controller('ProductController', function (productDAO) {
+module.factory('categoryDAO', function ($resource) {
+    return $resource('/api/categories/:cat');
+});
 
+// Product controller
+module.controller('ProductController', function (productDAO, categoryDAO) {
+
+    // load the products
+    this.products = productDAO.query();
+
+    // load the categories
+    this.categories = categoryDAO.query();
+
+    // click handler for the category filter buttons
+    this.selectCategory = function (selectedCat) {
+        this.products = categoryDAO.query({"cat": selectedCat});
+    };
+    
+    this.displayAll = function () {
+        this.products = productDAO.query();
+    };
+    
 });
 
 // Customer controller
+
 
 // Shopping cart controller
